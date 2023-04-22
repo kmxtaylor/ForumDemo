787aeef4649f385ffe468e78641f7d0e49afdf61
Katie Taylor <katiemxtaylor@gmail.com>
2023-04-22 02:33:12 -0700

    load fonts & splash screen

--- App.js
+++ App.js
@@ -6,17 +6,43 @@ import {
   StatusBar,
 } from 'react-native';
 
-import { useFonts, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold } from '@expo-google-fonts/rubik';
+import {
+  useFonts,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_700Bold
+} from '@expo-google-fonts/rubik';
 import * as SplashScreen from 'expo-splash-screen';
 
 import MainScreen from './screens/mainScreen';
 
+SplashScreen.preventAutoHideAsync();
+
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   
   return (
     <>
       <StatusBar barStyle='light-content' />
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView
+        style={styles.container}
+        onLayout={onLayoutRootView}
+        testID='app-screen'
+      >
         <KeyboardAvoidingView
           style={styles.subContainer}
           behavior='padding'
