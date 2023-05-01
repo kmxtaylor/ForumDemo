dce53078cb9452f3b4d925eff222e134b411aaf3
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-01 13:16:46 -0700

    systematically update styles in deletion modal

--- screens/Forum/DeletionModal.js
+++ screens/Forum/DeletionModal.js
@@ -21,31 +21,33 @@ const deletionModal = ({
       transparent={true}
       visible={isModalVisible}
     >
-      <View style={styles.modal}>
-        <MyCard style={styles.modalContent}>
-          
-          <MyText style={styles.modalTitle}>Delete comment</MyText>
-          
-          <MyText style={styles.modalText}> 
-            Are you sure you want to delete this comment?
-            This will remove the comment and can't be undone.
-          </MyText>
-          
-          <View style={styles.modalButtonContainer}>
-          
-            {/* marginRight & marginLeft is used to help with button spacing */}
-            <MyButton
-                onPress={cancelDeletePost} 
-                style={[styles.modalButton, {backgroundColor: colors.neutral.grayishBlue}, {marginLeft: 15}]}>
-                <MyText style={styles.modalButtonText}>NO, CANCEL</MyText>
-            </MyButton>
+      <View style={styles.modalOverlay}>
+        <MyCard>
+          <View style={styles.modalContent}>
+            <View>
+              <MyText style={styles.modalTitleText}>Delete comment</MyText>
+            </View>
+            <View>
+              <MyText style={styles.modalBodyText}> 
+                Are you sure you want to delete this comment?
+                This will remove the comment and can't be undone.
+              </MyText>
+            </View>
             
-            <MyButton
-              onPress={confirmDeletePost} 
-              style={[styles.modalButton, {backgroundColor: colors.primary.softRed, marginRight: 15}]}>  
-              <MyText style={styles.modalButtonText}>YES, DELETE</MyText>
-            </MyButton>
-            
+            <View style={styles.modalButtonContainer}>
+              {/* marginRight & marginLeft is used to help with button spacing */}
+              <MyButton
+                  onPress={cancelDeletePost} 
+                  style={[styles.modalButton, {backgroundColor: colors.neutral.grayishBlue}]}>
+                  <MyText style={styles.modalButtonText}>NO, CANCEL</MyText>
+              </MyButton>
+              
+              <MyButton
+                onPress={confirmDeletePost} 
+                style={[styles.modalButton, {backgroundColor: colors.primary.softRed}]}>  
+                <MyText style={styles.modalButtonText}>YES, DELETE</MyText>
+              </MyButton>
+            </View>
           </View>
         </MyCard>
       </View>
@@ -55,57 +57,58 @@ const deletionModal = ({
 
 const styles = StyleSheet.create({
   // Modal styles 
-  modal: {
+  modalOverlay: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)', // 50% opacity black background   
+    backgroundColor: 'rgba(0,0,0,0.5)', // 50% opacity black background
+    padding: 15,
   },
   // Pop-up modal container
   modalContent: {
-    height: 227,
-    width: 380,
-    //padding: 20,
-    backgroundColor:'hsl(0, 0%, 100%)',
-    borderRadius: 6,
+    width: '100%',
+    // padding: 15,
+    padding: 10,
+    // padding: 5,
+    rowGap: 15,
   },
   // Modal header
-  modalTitle: {
+  modalTitleText: {
     fontSize: 20,
-    fontFamily: 'Rubik_500Medium',
-    marginRight: 164,
-    marginBottom: 20,
-    color: colors.red,
+    fontWeight: 500,
+    // marginBottom: 20,
+    textAlign: 'left',
   },
   // Modal text (body)
-  modalText: {
-    fontSize: 17,
-    fontFamily: 'Rubik_400Regular',
+  modalBodyText: {
+    fontSize: 16, // style guide says 16px for body/paragraph
+    fontWeight: 400,
+    // letterSpacing: 0.5,
+    lineHeight: 24,
     textAlign: 'left',
-    marginRight: 10,
-    marginBottom: 20,
-    letterSpacing: 0.8,
-    color: ' hsl(211, 10%, 45%)' // Grayish blue text
+    color: colors.neutral.grayishBlue,
+    // marginBottom: 15,
   },
   // Modal buttons container
   modalButtonContainer: {
     flexDirection: 'row',
+    // justifyContent: 'center',
+    justifyContent: 'space-between',
     width: '100%',
   },
   // Modal buttons (Cancel and Delete)
   modalButton: {
-    width: 150,
-    height: 50,
-    marginHorizontal: 8, //margin between buttons
-    borderRadius: 10,
-    padding: 10,
-    alignItems: 'center',
+    // width: 133,
+    width: '48%',
+    height: 45,
+    borderRadius: 8,
+    // marginHorizontal: 6,
   },
   // Modal button text
   modalButtonText: {
     fontSize: 16,
-    fontFamily: 'Rubik_500Medium',
-    color: "hsl(0, 0%, 100%)"
+    fontWeight: 500,
+    color: colors.neutral.white,
   },
 });
 
