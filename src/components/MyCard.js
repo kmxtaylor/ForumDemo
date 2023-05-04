ee784dc83613f93e231182550f64b1ea402ee09e
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-03 20:48:58 -0700

    restructure project w/ ./src base path
    enable absolute paths

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,7 +1,7 @@
 import { View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
-import colors from '../assets/colors';
+import colors from '../../assets/colors';
 
 /**
  * Custom card container
