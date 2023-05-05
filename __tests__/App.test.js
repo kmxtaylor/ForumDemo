656d7e0a6bdf7830f5b56f1a1933e6fc3aeaf23a
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-05 14:45:55 -0700

    only rendering in waitFor
    getBy are easier to debug outside of waitFor

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -5,25 +5,24 @@ const TIMEOUT = { timeout: 20000 };
 
 describe('Overall app', () => {
   // test if the app renders correctly without crashing: jest-expo is required
-  test('renders correctly', async () =>  {
-    const { getByTestId } = render(<App />);
-    let asyncContent;
-
+  test('should render the app screen', async () => {
+    let renderedApp;
     await waitFor(() => {
-      asyncContent = getByTestId('app-screen');
-    }, TIMEOUT);
-
-    expect(asyncContent).toBeDefined();
+        renderedApp = render(<App />);
+      });
+    const { getByTestId } = renderedApp;
+    const appScreen = getByTestId('app-screen');
+  
+    expect(appScreen).toBeDefined();
   });
+  // test('renders correctly', async () =>  {
+  //   const { getByTestId } = render(<App />);
+  //   let asyncContent;
 
-  // test('should render the app screen', async () => {
-  //   let renderedComponent;
   //   await waitFor(() => {
-  //     renderedComponent = render(<App />);
-  //   });
-  //   const { getByTestId } = renderedComponent;
-  //   const appScreen = getByTestId('app-screen');
+  //     asyncContent = getByTestId('app-screen');
+  //   }, TIMEOUT);
 
-  //   expect(appScreen).toBeDefined();
+  //   expect(asyncContent).toBeDefined();
   // });
 });
--- __tests__/screens/Forum/index.test.js
+++ __tests__/screens/Forum/index.test.js
@@ -5,32 +5,6 @@ import App from '/';
 const TIMEOUT = { timeout: 20000 };
 
 describe('Forum functionality', () => {
-  // test if can add comment
-  test('comment adding correctly', async () =>  {
-    const { getByTestId } = render(<Forum />);
-    let input, commentsList, sendButton;
-
-    await waitFor(() => {
-      // access all testIDs here
-      input = getByTestId('input');
-      commentsList = getByTestId('comments-list');
-      sendButton = getByTestId('submit-button');
-    }, TIMEOUT);
-
-    expect(input).toBeDefined();
-    expect(commentsList).toBeDefined();
-
-    const TEST_COMMENT_TEXT = 'test comment';
-    fireEvent.changeText(input, TEST_COMMENT_TEXT);
-    fireEvent.press(sendButton);
-
-    // console.log(commentsList.props);
-    const [ lastComment ] = commentsList.props.data.slice(-1);
-    // const [ lastComment ] = commentsList.props.data.reverse();
-
-    expect(lastComment.content).toBe(TEST_COMMENT_TEXT);
-  });
-
   // test('renders correctly - from app', async () =>  {
   //   const { getByTestId } = render(<App />);
   //   let input, commentsList;
@@ -44,5 +18,89 @@ describe('Forum functionality', () => {
   //   expect(commentsList).toBeDefined();
   // });
 
-  
+  // test if can add comment
+  test('comment adding correctly', async () =>  {
+    let renderedScreen;
+    await waitFor(() => {
+      renderedScreen = render(<Forum />);
+    }, TIMEOUT);
+    const { getByTestId } = renderedScreen;
+
+    const input = getByTestId('input');
+    const commentsList = getByTestId('comments-list');
+    const sendButton = getByTestId('submit-button');
+
+    // expect(input).toBeDefined();
+    // expect(commentsList).toBeDefined();
+
+    const COMMENT_TEXT = 'test comment';
+    fireEvent.changeText(input, COMMENT_TEXT);
+    fireEvent.press(sendButton);
+
+    console.log(commentsList.props);
+    const [ lastComment ] = commentsList.props.data.slice(-1);
+    // const [ lastComment ] = commentsList.props.data.reverse();
+
+    expect(lastComment.content).toBe(COMMENT_TEXT);
+  });
+  // test('comment adding correctly', async () =>  {
+  //   const { getByTestId } = render(<Forum />);
+  //   let input, commentsList, sendButton;
+
+  //   await waitFor(() => {
+  //     // access all testIDs here
+  //     input = getByTestId('input');
+  //     commentsList = getByTestId('comments-list');
+  //     sendButton = getByTestId('submit-button');
+  //   }, TIMEOUT);
+
+  //   // expect(input).toBeDefined();
+  //   // expect(commentsList).toBeDefined();
+
+  //   const COMMENT_TEXT = 'test comment';
+  //   fireEvent.changeText(input, COMMENT_TEXT);
+  //   fireEvent.press(sendButton);
+
+  //   console.log(commentsList.props);
+  //   const [ lastComment ] = commentsList.props.data.slice(-1);
+  //   // const [ lastComment ] = commentsList.props.data.reverse();
+
+  //   expect(lastComment.content).toBe(COMMENT_TEXT);
+  // });
+
+  /* test if can reply to comment */
+  // test('reply correctly', async () =>  {
+  //   let renderedScreen;
+  //   await waitFor(() => {
+  //     renderedScreen = render(<Forum />);
+  //   }, TIMEOUT);
+  //   const { getByTestId, getByType } = renderedScreen;
+    
+  //   const input = getByTestId('input');
+  //   const commentsList = getByTestId('comments-list');
+  //   // const [ firstComment ] = commentsList.props.data;
+  //   // replyButton = getByTestId(`reply-button-${firstComment.id}`);
+  //   const replyButton = getByType(commentsList[0], 'Button');
+  //   console.log(replyButton);
+  //   const sendButton = getByTestId('submit-button');
+
+  //   // expect(input).toBeDefined();
+  //   // expect(commentsList).toBeDefined();
+
+  //   // access the reply button on the first comment
+  //   fireEvent.press(replyButton);
+
+  //   const REPLY_TEXT = 'test comment';
+  //   fireEvent.changeText(input, REPLY_TEXT);
+  //   fireEvent.press(sendButton);
+
+  //   // console.log(commentsList.props);
+  //   const [ firstComment ] = commentsList.props.data;
+
+  //   expect(firstComment.content).toBe(REPLY_TEXT);
+  // });
+
+  // test if can reply to reply 
+
+
 });
