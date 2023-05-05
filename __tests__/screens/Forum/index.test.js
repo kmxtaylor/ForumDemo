c452927cf7d286a98fd3ddd053bf2f1431524016
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-05 14:10:06 -0700

    correctly test comment adding functionality

--- __tests__/screens/Forum/index.test.js
+++ __tests__/screens/Forum/index.test.js
@@ -5,18 +5,30 @@ import App from '/';
 const TIMEOUT = { timeout: 20000 };
 
 describe('Forum functionality', () => {
-  // test if reply functionality works correctly
-  test('renders correctly', async () =>  {
+  // test if can add comment
+  test('comment adding correctly', async () =>  {
     const { getByTestId } = render(<Forum />);
-    let input, commentsList;
+    let input, commentsList, sendButton;
 
     await waitFor(() => {
+      // access all testIDs here
       input = getByTestId('input');
       commentsList = getByTestId('comments-list');
+      sendButton = getByTestId('submit-button');
     }, TIMEOUT);
 
     expect(input).toBeDefined();
     expect(commentsList).toBeDefined();
+
+    const TEST_COMMENT_TEXT = 'test comment';
+    fireEvent.changeText(input, TEST_COMMENT_TEXT);
+    fireEvent.press(sendButton);
+
+    // console.log(commentsList.props);
+    const [ lastComment ] = commentsList.props.data.slice(-1);
+    // const [ lastComment ] = commentsList.props.data.reverse();
+
+    expect(lastComment.content).toBe(TEST_COMMENT_TEXT);
   });
 
   // test('renders correctly - from app', async () =>  {
