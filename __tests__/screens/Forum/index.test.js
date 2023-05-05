71e0c6bb52df471422cf0a76d896bd0f101dbda0
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-05 13:32:42 -0700

    fix test: render other components

--- __tests__/screens/Forum/index.test.js
+++ __tests__/screens/Forum/index.test.js
@@ -1,11 +1,36 @@
-import { render, fireEvent } from '@testing-library/react-native';
-import Forum from '../../../screens/Forum';
-import App from '../App';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import Forum from 'screens/Forum';
+import App from '/';
 
-// Functionality to test:
+const TIMEOUT = { timeout: 20000 };
 
-describe('MainScreen', () => {
+describe('Forum functionality', () => {
+  // test if reply functionality works correctly
+  test('renders correctly', async () =>  {
+    const { getByTestId } = render(<Forum />);
+    let input, commentsList;
 
+    await waitFor(() => {
+      input = getByTestId('input');
+      commentsList = getByTestId('comments-list');
+    }, TIMEOUT);
 
+    expect(input).toBeDefined();
+    expect(commentsList).toBeDefined();
+  });
 
+  // test('renders correctly - from app', async () =>  {
+  //   const { getByTestId } = render(<App />);
+  //   let input, commentsList;
+
+  //   await waitFor(() => {
+  //     input = getByTestId('input');
+  //     commentsList = getByTestId('comments-list');
+  //   }, TIMEOUT);
+
+  //   expect(input).toBeDefined();
+  //   expect(commentsList).toBeDefined();
+  // });
+
+  
 });
