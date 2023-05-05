c2b2e0d2b192c4c7c3b18228ee40aee3335e0f39
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-05 13:16:40 -0700

    fix basic render test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,18 +1,29 @@
 import { render, fireEvent, waitFor } from '@testing-library/react-native';
 import App from '../App';
-// import MainScreen from '../screens/MainScreen';
-import CommentsList from '../screens/Forum/CommentsList';
 
-describe('Whole app', () => {
-  // test if the app renders correctly
-  test('should render the app screen', async () => {
-    let renderedComponent;
+const TIMEOUT = { timeout: 20000 };
+
+describe('Overall app', () => {
+  // test if the app renders correctly without crashing: jest-expo is required
+  test('renders correctly', async () =>  {
+    const { getByTestId } = render(<App />);
+    let asyncContent;
+
     await waitFor(() => {
-      renderedComponent = render(<App />);
-    });
-    const { getByTestId } = renderedComponent;
-    const appScreen = getByTestId('app-screen');
+      asyncContent = getByTestId('app-screen');
+    }, TIMEOUT);
 
-    expect(appScreen).toBeDefined();
+    expect(asyncContent).toBeDefined();
   });
+
+  // test('should render the app screen', async () => {
+  //   let renderedComponent;
+  //   await waitFor(() => {
+  //     renderedComponent = render(<App />);
+  //   });
+  //   const { getByTestId } = renderedComponent;
+  //   const appScreen = getByTestId('app-screen');
+
+  //   expect(appScreen).toBeDefined();
+  // });
 });
